b7651d8a6d8da68ed7fb51a12a16c87662147629
Danny de Jonge <dannydejonge1796@gmail.com>
2023-02-05 13:36:40 +0100

    customer class voorzien van comments

--- src/main/java/energie/models/Customer.java
+++ src/main/java/energie/models/Customer.java
@@ -28,21 +28,29 @@ public class Customer {
   private ArrayList<GasRate> gasRates;
   private ArrayList<WeeklyUsage> weeklyUsages;
 
-  public Customer(String customerNr, String firstname, String lastname, Double advance) {
+  public Customer(String customerNr, String firstname, String lastname, Double advance)
+  {
     this.customerNr = customerNr;
     this.firstname = firstname;
     this.lastname = lastname;
     this.advance = advance;
+    //Haal alle stroomtarieven van de klant op en stop ze in de arraylist
     initElectricityRates();
+    //Haal alle gastarieven van de klant op en stop ze in de arraylist
     initGasRates();
+    //Haal alle wekelijkse gebruiken van de klant op en stop ze in de arraylist
     initWeeklyUsage();
   }
 
   public TableView<WeeklyUsage> getTableUsage()
   {
+    //Maak een nieuwe tabel voor wekelijks energieverbruik
     TableView<WeeklyUsage> table = new TableView<>();
+    //Vul de tabel met items uit de wekelijkse energieverbruik lijst
     table.setItems(FXCollections.observableArrayList(this.weeklyUsages));
 
+    //Maak kolommen aan voor alle properties
+    //Vul de kolommen met de waarden uit de properties
     TableColumn<WeeklyUsage, String> colUsageElec = new TableColumn<>("Stroom gebruik");
     colUsageElec.setCellValueFactory(new PropertyValueFactory<>("usageElec"));
 
@@ -55,52 +63,65 @@ public class Customer {
     TableColumn<WeeklyUsage, String> colDateEnd = new TableColumn<>("Eind datum");
     colDateEnd.setCellValueFactory(new PropertyValueFactory<>("dateEnd"));
 
+    //Maak een kolom voor deleteknoppen
     TableColumn<WeeklyUsage, Void> deleteCol = new TableColumn<>("Delete");
+    //Maak een knop in de cel van de kolom
     deleteCol.setCellFactory(new Callback<>() {
       @Override
       public TableCell<WeeklyUsage, Void> call(final TableColumn<WeeklyUsage, Void> param) {
         return new TableCell<>() {
           private final Button deleteButton = new Button("Delete");
-
+          //Als op de verwijderknop wordt geklikt
           {
             deleteButton.setOnAction((ActionEvent event) -> {
+              //Haal het wekelijkse energieverbruik op uit de tabel
               WeeklyUsage weeklyUsage = getTableView().getItems().get(getIndex());
+              //Verwijder wekelijks verbruik uit database
               weeklyUsage.destroy();
+              //Verwijder wekelijks gebruik uit arraylist van customer
               weeklyUsages.remove(weeklyUsage);
+              //Verwijder de row uit de tabel
               table.getItems().remove(weeklyUsage);
             });
           }
 
           @Override
+          //Deze functie houdt de deleteknop kolom up-to-date
           public void updateItem(Void item, boolean empty) {
             super.updateItem(item, empty);
             if (empty) {
+              //Als de rij leeg is, verwijder de deleteknop
               setGraphic(null);
             } else {
+              //Als de rij niet leeg is, zet dan de deleteknop als het grafische element
               setGraphic(deleteButton);
             }
           }
         };
       }
     });
-
+    //Voeg alle kolommen toe aan de tabel
     table.getColumns().add(colUsageElec);
     table.getColumns().add(colUsageGas);
     table.getColumns().add(colDateStart);
     table.getColumns().add(colDateEnd);
     table.getColumns().add(deleteCol);
-
+    //De kolommen hebben een statische breedte en krijgen dze automatisch
     table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     table.autosize();
-
+    //Geef de tabel terug
     return table;
   }
 
   public TableView<ElectricityRate> getTableElec()
   {
+    //Maak een nieuwe tabel voor stroomtarief
     TableView<ElectricityRate> table = new TableView<>();
+    //Vul de tabel met items uit de lijst met stroomtarieven
     table.setItems(FXCollections.observableArrayList(this.electricityRates));
 
+    //Maak kolommen aan voor alle properties
+    //Vul de kolommen met de waarden uit de properties
     TableColumn<ElectricityRate, String> colRate = new TableColumn<>("Stroom tarief");
     colRate.setCellValueFactory(new PropertyValueFactory<>("rate"));
 
@@ -110,51 +131,64 @@ public class Customer {
     TableColumn<ElectricityRate, String> colDateTo = new TableColumn<>("Datum tot");
     colDateTo.setCellValueFactory(new PropertyValueFactory<>("dateTo"));
 
+    //Maak een kolom voor deleteknoppen
     TableColumn<ElectricityRate, Void> deleteCol = new TableColumn<>("Delete");
+    //Maak een knop in de cel van de kolom
     deleteCol.setCellFactory(new Callback<>() {
       @Override
       public TableCell<ElectricityRate, Void> call(final TableColumn<ElectricityRate, Void> param) {
         return new TableCell<>() {
           private final Button deleteButton = new Button("Delete");
-
+          //Als op de verwijderknop wordt geklikt
           {
             deleteButton.setOnAction((ActionEvent event) -> {
+              //Haal het stroomtarief op uit de tabel
               ElectricityRate electricityRate = getTableView().getItems().get(getIndex());
+              //Verwijder het stroomtarief uit de database
               electricityRate.destroy();
+              //Verwijder het stroomtarief uit customer
               electricityRates.remove(electricityRate);
+              //Verwijder het stroomtarief uit de tabel
               table.getItems().remove(electricityRate);
             });
           }
 
           @Override
+          //Deze functie houdt de deleteknop kolom up-to-date
           public void updateItem(Void item, boolean empty) {
             super.updateItem(item, empty);
             if (empty) {
+              //Als de rij leeg is, verwijder de deleteknop
               setGraphic(null);
             } else {
+              //Als de rij niet leeg is, zet dan de deleteknop als het grafische element
               setGraphic(deleteButton);
             }
           }
         };
       }
     });
-
+    //Voeg alle kolommen toe aan de tabel
     table.getColumns().add(colRate);
     table.getColumns().add(colDateFrom);
     table.getColumns().add(colDateTo);
     table.getColumns().add(deleteCol);
-
+    //De kolommen hebben een statische breedte en krijgen dze automatisch
     table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     table.autosize();
-
+    //Geef de tabel terug
     return table;
   }
 
   public TableView<GasRate> getTableGas()
   {
+    //Maak een nieuwe tabel voor gastarief
     TableView<GasRate> table = new TableView<>();
+    //Vul de tabel met items uit de lijst met gastarieven
     table.setItems(FXCollections.observableArrayList(this.gasRates));
 
+    //Maak kolommen aan voor alle properties
+    //Vul de kolommen met de waarden uit de properties
     TableColumn<GasRate, String> colRate = new TableColumn<>("Gas tarief");
     colRate.setCellValueFactory(new PropertyValueFactory<>("rate"));
 
@@ -164,43 +198,52 @@ public class Customer {
     TableColumn<GasRate, String> colDateTo = new TableColumn<>("Datum tot");
     colDateTo.setCellValueFactory(new PropertyValueFactory<>("dateTo"));
 
+    //Maak een kolom voor deleteknoppen
     TableColumn<GasRate, Void> deleteCol = new TableColumn<>("Delete");
+    //Maak een knop in de cel van de kolom
     deleteCol.setCellFactory(new Callback<>() {
       @Override
       public TableCell<GasRate, Void> call(final TableColumn<GasRate, Void> param) {
         return new TableCell<>() {
           private final Button deleteButton = new Button("Delete");
-
+          //Als op de verwijderknop wordt geklikt
           {
             deleteButton.setOnAction((ActionEvent event) -> {
+              //Haal het gastarief op uit de tabel
               GasRate gasRate = getTableView().getItems().get(getIndex());
+              //Verwijder het gastarief uit de database
               gasRate.destroy();
+              //Verwijder het gastarief uit de customer
               gasRates.remove(gasRate);
+              //Verwijder het gastarief uit de tabel
               table.getItems().remove(gasRate);
             });
           }
 
           @Override
+          //Deze functie houdt de deleteknop kolom up-to-date
           public void updateItem(Void item, boolean empty) {
             super.updateItem(item, empty);
             if (empty) {
+              //Als de rij leeg is, verwijder de deleteknop
               setGraphic(null);
             } else {
+              //Als de rij niet leeg is, zet dan de deleteknop als het grafische element
               setGraphic(deleteButton);
             }
           }
         };
       }
     });
-
+    //Voeg alle kolommen toe aan de tabel
     table.getColumns().add(colRate);
     table.getColumns().add(colDateFrom);
     table.getColumns().add(colDateTo);
     table.getColumns().add(deleteCol);
-
+    //De kolommen hebben een statische breedte en krijgen dze automatisch
     table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     table.autosize();
-
+    //Geef de tabel terug
     return table;
   }
 
@@ -211,18 +254,21 @@ public class Customer {
     if (selectedPeriod.equals("Wekelijks")) {
       //Haal het resultaat op per week
       divideAdvance = 52;
+      //Group by string voor wekelijks
       groupString = "GROUP BY WEEK(weekly_usage.date_start) ";
     }
 
     if (selectedPeriod.equals("Maandelijks")) {
       //Haal het resultaat op per maand
       divideAdvance = 12;
+      //Group by string voor maandelijks
       groupString = "GROUP BY MONTH(weekly_usage.date_start) ";
     }
 
     if (selectedPeriod.equals("Jaarlijks")) {
       //Haal het resultaat op per jaar
       divideAdvance = 1;
+      //Group by string voor jaarlijks
       groupString = "GROUP BY YEAR(weekly_usage.date_start) ";
     }
 
@@ -247,11 +293,11 @@ public class Customer {
     //Kolom voorschot / bepaalde periode
     "ROUND(customer.advance / "+divideAdvance+", 2) as Average_advance, " +
     //Kolom overschrijding, voorschot - totale kosten
-    "ROUND((SUM(weekly_usage.usage_elec * electricity_rate.rate) + SUM(weekly_usage.usage_gas * gas_rate.rate)) - (customer.advance / "+divideAdvance+"), 2) as Exceedance " +
+    "ROUND((SUM(weekly_usage.usage_elec * electricity_rate.rate) + SUM(weekly_usage.usage_gas * gas_rate.rate)) - (customer.advance / "+divideAdvance+"), 2) as Exceed " +
     //Van tabel (wekelijks)gebruik met joins (rates tabellen en customer tabel)
     //Gebruik left join zodat de weekly usage wordt weergegeven als rate of advance niet bestaat
     "FROM weekly_usage " +
-    //Join elektarief als de periode van het (wekelijks)gebruik binnen de periode van het tarief valt, als er meerdere zijn geef de laatst toegevoegde
+    //Join stroomtarief als de periode van het (wekelijks)gebruik binnen de periode van het tarief valt, als er meerdere zijn geef de laatst toegevoegde
     "LEFT JOIN electricity_rate " +
     "ON electricity_rate.id = (SELECT MAX(id) " +
     "FROM electricity_rate er " +
@@ -281,11 +327,16 @@ public class Customer {
     //Sort by most recent start date
     "ORDER BY MIN(weekly_usage.date_start) DESC";
 
+    //Maak een tabel aan voor gejoinde result
     TableView<ObservableList<String>> tableView = new TableView<>();
     tableView.setEditable(true);
 
+    //Aanmaken observable list met daarin een observable list die alle strings van een row bevat
     ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
 
+    //Maak kolom aan
+    //Verkrijg de waarde uit de observable list die alle strings van een row bevat
+    //Dit voor elke property van het gejoinde resultaat
     TableColumn<ObservableList<String>, String> column0 = new TableColumn<>("Periode");
     column0.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().get(0)));
     TableColumn<ObservableList<String>, String> column1 = new TableColumn<>("Gebruik elek");
@@ -307,6 +358,7 @@ public class Customer {
     TableColumn<ObservableList<String>, String> column9 = new TableColumn<>("Overschrijding");
     column9.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().get(9)));
 
+    //Voeg alle kolommen toe aan de tabel
     tableView.getColumns().add(column0);
     tableView.getColumns().add(column1);
     tableView.getColumns().add(column2);
@@ -317,15 +369,19 @@ public class Customer {
     tableView.getColumns().add(column7);
     tableView.getColumns().add(column8);
     tableView.getColumns().add(column9);
-
+    //Stel de items in met de lijst van rows
     tableView.setItems(data);
-
+    //Haal het resultaat op uit de database
     ResultSet result = Application.db.getData(query);
-
+    //Check of alles goed gaat met de database, anders mysql exception
     try {
+      //Voor elk resultaat
       while (result.next()) {
+        //Maak een nieuwe row aan als observable list
         ObservableList<String> row = FXCollections.observableArrayList();
-
+        //Haal alle kolommen uit het resultaat en zet ze in de row list
+        //Het is een list met strings, dus van de resultaten moet een string worden gemaakt
+        //Als een kolom 0.0 als resultaat geeft, stop dan "Onbekend" in de row
         String period = String.valueOf(result.getString("Period"));
         row.add(period);
         String usageElec = String.valueOf(result.getInt("Total_elec_usage"));
@@ -344,71 +400,87 @@ public class Customer {
         row.add(costTotal);
         String averageAdvance = String.valueOf(result.getFloat("Average_advance")).equals("0.0") ? "Onbekend" : String.valueOf(result.getFloat("Average_advance"));
         row.add(averageAdvance);
-        String exceedance = String.valueOf(result.getFloat("Exceedance")).equals("0.0") ? "Onbekend" : String.valueOf(result.getFloat("Exceedance"));
-        row.add(exceedance);
-
+        String exceed = String.valueOf(result.getFloat("Exceed")).equals("0.0") ? "Onbekend" : String.valueOf(result.getFloat("Exceed"));
+        row.add(exceed);
+        //Voeg de row toe aan de lijst met rows
         data.add(row);
       }
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-
+    //Geef de tabel terug
     return tableView;
   }
 
   public WeeklyUsage getCurrentWeeklyUsage()
   {
+    //Initieer met null als value
     WeeklyUsage result = null;
 
+    //Loop door de lijst
     for (WeeklyUsage weeklyUsage : this.weeklyUsages) {
+      //Als de einddatum van het wekelijks gebruik in de periode van nu min een week ligt
       if (!weeklyUsage.getDateEnd().isBefore(LocalDate.now().minusWeeks(1)) && !weeklyUsage.getDateEnd().isAfter(LocalDate.now())) {
+        //dan is dat het huidige wekelijkse verbruik
         result = weeklyUsage;
       }
     }
+
+    //Geef huidige wekelijkse verbruik terug of null
     return  result;
   }
 
   public ElectricityRate getCurrentElecRate()
   {
+    //Initieer met null als value
     ElectricityRate result = null;
 
     for (ElectricityRate electricityRate : this.electricityRates) {
+      //Als de huidige datum binnen periode van tarief ligt
       if (!LocalDate.now().isBefore(electricityRate.getDateFrom()) && !LocalDate.now().isAfter(electricityRate.getDateTo())) {
+        //dan is dat het huidige tarief
         result = electricityRate;
       }
     }
 
+    //Geef huidige tarief terug of null
     return result;
   }
 
   public GasRate getCurrentGasRate()
   {
+    //Initieer met null als value
     GasRate result = null;
 
     for (GasRate gasRate : this.gasRates) {
+      //Als de huidige datum binnen periode van tarief ligt
       if (!LocalDate.now().isBefore(gasRate.getDateFrom()) && !LocalDate.now().isAfter(gasRate.getDateTo())) {
+        //dan is dat het huidige tarief
         result = gasRate;
       }
     }
 
+    //Geef huidige tarief terug of null
     return result;
   }
 
   private void initElectricityRates()
   {
+    //Maak nieuwe arraylist aan
     this.electricityRates = new ArrayList<>();
-
+    //Stel een decimalformat in
     DecimalFormat decimalFormat = new DecimalFormat("#.##");
-
+    //Query om alle stroomtarieven van een gebruiker op te halen, order datum vanaf meest recent boven
     String query =
             "SELECT * " +
             "FROM electricity_rate " +
             "WHERE customer_number = '"+customerNr+"' " +
             "ORDER BY electricity_rate.date_from DESC";
-
+    //Haal resultaat op uit db
     ResultSet result = Application.db.getData(query);
 
     try {
+      //Voor elk resultaat, maak nieuwe instantie van stroomtarief en voeg toe aan arraylist
       while (result.next()) {
         this.electricityRates.add(new ElectricityRate(
                 result.getString("customer_number"),
@@ -424,19 +496,21 @@ public class Customer {
 
   private void initGasRates()
   {
+    //Maak nieuwe arraylist aan
     this.gasRates = new ArrayList<>();
-
+    //Stel een decimalformat in
     DecimalFormat decimalFormat = new DecimalFormat("#.##");
-
+    //Query om alle gastarieven van een gebruiker op te halen, order datum vanaf meest recent boven
     String query =
             "SELECT * " +
             "FROM gas_rate " +
             "WHERE customer_number = '"+customerNr+"' " +
             "ORDER BY gas_rate.date_from DESC";
-
+    //Haal resultaat op uit db
     ResultSet result = Application.db.getData(query);
 
     try {
+      //Voor elk resultaat, maak nieuwe instantie van gastarief en voeg toe aan arraylist
       while (result.next()) {
         this.gasRates.add(new GasRate(
                 result.getString("customer_number"),
@@ -452,17 +526,19 @@ public class Customer {
 
   private void initWeeklyUsage()
   {
+    //Maak nieuwe arraylist aan
     this.weeklyUsages = new ArrayList<>();
-
+    //Query om alle wekelijkse gebruiken van een gebruiker op te halen, order datum start meest recent boven
     String query =
             "SELECT * " +
             "FROM weekly_usage " +
             "WHERE customer_number = '"+customerNr+"' " +
             "ORDER BY weekly_usage.date_start DESC";
-
+    //Haal resultaat op uit db
     ResultSet result = Application.db.getData(query);
 
     try {
+      //Voor elk resultaat, maak nieuwe instantie van wekelijks gebruik en voeg toe aan arraylist
       while (result.next()) {
         this.weeklyUsages.add(new WeeklyUsage(
                 result.getString("customer_number"),
@@ -479,38 +555,44 @@ public class Customer {
 
   public void store()
   {
+    //Query om een customer op te slaan in db
     String query =
             "INSERT INTO customer (number, firstname, lastname) " +
                     "VALUES ('" + customerNr + "', '" + firstname + "', '" + lastname + "')";
 
+    //Roep store functie in db class
     Application.db.storeData(query);
   }
 
   public void update()
   {
+    //Query om een customer up-te-daten
     String query =
             "UPDATE customer " +
                     "SET firstname = '" + firstname + "', lastname = '" + lastname + "', advance = '" + advance + "'" +
                     "WHERE number = '" + customerNr + "'";
 
-
+    //Roep store functie aan in db
     Application.db.storeData(query);
   }
 
   public void addToElectricityRates(ElectricityRate electricityRate) {
+    //Voeg stroomtarief toe aan lijst met stroomtarieven
     this.electricityRates.add(electricityRate);
   }
 
   public void addToGasRates(GasRate gasRate) {
+    //Voeg gastarief toe aan lijst met gastarieven
     this.gasRates.add(gasRate);
+  }
+
+  public void addToWeeklyUsages(WeeklyUsage weeklyUsage) {
+    //Voeg wekelijks gebruik toe aan lijst met wekelijkse gebruiken
+    this.weeklyUsages.add(weeklyUsage);
   }
 
   public void setAdvance(Double advance) {
     this.advance = advance;
-  }
-
-  public void addToWeeklyUsages(WeeklyUsage weeklyUsage) {
-    this.weeklyUsages.add(weeklyUsage);
   }
 
   public String getCustomerNr() {
